4d5db14011a87cb308c1b8d2a1281319847ba6cc
Biju Balakrishnan <bijukbcan@gmail.com>
2020-04-09 14:49:20 -0400

    added new table - Employee

---
+++ Staging/Staging/Employee.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[EmployeeId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(MAX) NULL, 
+    [LastName] NVARCHAR(MAX) NULL, 
+    [EmployeeNumber] NVARCHAR(MAX) NULL, 
+    [Gender] NCHAR(2) NULL, 
+    [JobTitle] NVARCHAR(MAX) NULL, 
+    [DateOfHire] DATETIME NULL
+)
